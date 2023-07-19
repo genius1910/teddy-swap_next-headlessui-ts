6740b558de1d08a0f8b248a7b3ff37a35186c6b3
genius1910 <geniusdev1910@skiff.com>
2023-07-19 21:29:49 +0600

    responsive issues fix for user

--- components/home/userinfo/User_Rightside.tsx
+++ components/home/userinfo/User_Rightside.tsx
@@ -63,62 +63,70 @@ const User_Rightside = () => {
     },
   ];
   return (
-    <div className="flex flex-col items-center w-full 2xl:w-auto mt-10 2xl:mt-4">
-      <TimeGroupButton />
-      {showComponent == "token-view-1" && (
-        <SelectTokenView
-          setSelectedToken={setSelectedToken}
-          setShowComponent={setShowComponent}
-        />
-      )}
-      {showComponent == "token-view-2" && (
-        <SelectTokenView
-          setSelectedToken={setSelectedToken2}
-          setShowComponent={setShowComponent}
-        />
-      )}
-      {showComponent == "view-1" ? (
-        <User_SwapTokens
-          setShowComponent={setShowComponent}
-          selectedToken2={selectedToken2}
-          selectedToken={selectedToken}
-        />
-      ) : (
-        ""
-      )}
-
-      <div className="list-component-color w-full 2xl:w-96 rounded-2xl mt-9">
-        <h2 className="p-6">Your Top Movers</h2>
-        <ul className="flex flex-col gap-0.5">
-          {movers.map((item, i) => {
-            return (
-              <li
-                key={item.name + i}
-                className="flex top-movers last:rounded-b-2xl justify-between px-4 py-2 last:mb-0"
-              >
-                <div className="flex gap-2 items-center">
-                  <Image
-                    className=" rounded-full"
-                    src={item.img}
-                    alt="Movers"
-                    width={36}
-                    height={36}
-                  />
-                  <div>
-                    <h1 className="text-sm uppercase font-medium">
-                      {item.name}
-                    </h1>
-                    <p className="text-[0.7rem] text-xs">{item.subname}</p>
+    <div className="flex flex-row 2xl:flex-col 2xl:items-center gap-6 w-full 2xl:w-auto mt-10 2xl:mt-4">
+      <div className="hidden 2xl:block">
+        <TimeGroupButton />
+      </div>
+      <div className="">
+        {showComponent == "token-view-1" && (
+          <SelectTokenView
+            setSelectedToken={setSelectedToken}
+            setShowComponent={setShowComponent}
+          />
+        )}
+        {showComponent == "token-view-2" && (
+          <SelectTokenView
+            setSelectedToken={setSelectedToken2}
+            setShowComponent={setShowComponent}
+          />
+        )}
+        {showComponent == "view-1" ? (
+          <User_SwapTokens
+            setShowComponent={setShowComponent}
+            selectedToken2={selectedToken2}
+            selectedToken={selectedToken}
+          />
+        ) : (
+          "Something wrong, please refresh the page"
+        )}
+      </div>
+      <div className="w-full flex justify-center items-center flex-col">
+        <div className="block 2xl:hidden">
+          <TimeGroupButton />
+        </div>
+        <div className="list-component-color w-full 2xl:w-96 rounded-2xl max-2xl:mt-6">
+          <h2 className="p-6">Your Top Movers</h2>
+          <ul className="flex flex-col gap-0.5">
+            {movers.map((item, i) => {
+              return (
+                <li
+                  key={item.name + i}
+                  className="flex top-movers last:rounded-b-2xl justify-between px-4 py-2 last:mb-0"
+                >
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      className=" rounded-full"
+                      src={item.img}
+                      alt="Movers"
+                      width={36}
+                      height={36}
+                    />
+                    <div>
+                      <h1 className="text-sm uppercase font-medium">
+                        {item.name}
+                      </h1>
+                      <p className="text-[0.7rem] text-xs">{item.subname}</p>
+                    </div>
                   </div>
-                </div>
-                <div className="text-right text-sm">
-                  <p>${item.price}</p>
-                  <p className=" text-[#16A34A] text-xs">+${item.up_price}</p>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                  <div className="text-right text-sm">
+                    <p>${item.price}</p>
+                    <p className=" text-[#16A34A] text-xs">+${item.up_price}</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
       {/* <User_RecentTransactions /> */}
     </div>
