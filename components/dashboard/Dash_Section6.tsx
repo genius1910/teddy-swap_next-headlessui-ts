439fad0723de2b7f3fce026f60528192b43ded9b
genius1910 <geniusdev1910@skiff.com>
2023-08-10 00:37:11 +0600

    content update

--- components/dashboard/Dash_Section6.tsx
+++ components/dashboard/Dash_Section6.tsx
@@ -9,7 +9,7 @@ const Dash_Section6 = () => {
         <h1 className="l-xl-heading">Making DeFi Secure for everyone</h1>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-14 lg:gap-24 text-lg 2xl:text-2xl items-center py-32 md:py-44 2xl:py-60">
-        <div className="col-span-1 lg:col-span-2 relative flex items-center justify-center lg:justify-end">
+        <div className="max-lg:hidden col-span-1 lg:col-span-2 relative flex items-center justify-center lg:justify-end">
           <Image
             src="/images/assets/global.svg"
             alt="Global image"
@@ -47,6 +47,15 @@ const Dash_Section6 = () => {
               available stablecoin pairs on our decentralized exchange.
             </h4>
           </div>
+        </div>
+        <div className="lg:hidden col-span-1 lg:col-span-2 relative flex items-center justify-center lg:justify-end">
+          <Image
+            src="/images/assets/global.svg"
+            alt="Global image"
+            width={520}
+            height={520}
+            className="w-10/12 drop-shadow-2xl"
+          />
         </div>
       </div>
     </section>
