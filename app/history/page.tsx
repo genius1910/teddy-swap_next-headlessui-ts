0c2b9f553293095b70134f8d83e65808da50d529
genius1910 <geniusdev1910@skiff.com>
2023-08-19 12:45:20 +0600

    responsive issues fix

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,13 +1,13 @@
-"use client"
-import History_Table from '@/components/history/History_Table'
-import React from 'react'
+"use client";
+import History_Table from "@/components/history/History_Table";
+import React from "react";
 
 const Index = () => {
-    return (
-        <div className='relative mt-20 '>
-            <History_Table />
-        </div>
-    )
-}
+  return (
+    <div className="relative mt-20 max-2xl:max-w-[1400px] mx-auto xl:zoom-80 2xl:zoom-100">
+      <History_Table />
+    </div>
+  );
+};
 
-export default Index
+export default Index;
