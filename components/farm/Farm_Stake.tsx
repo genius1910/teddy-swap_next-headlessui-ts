dad80a28bdf96fabadb2ac0b36687dd047afb5ed
genius1910 <geniusdev1910@skiff.com>
2023-09-04 20:19:06 -0400

    feat(update input): fix ui

--- components/farm/Farm_Stake.tsx
+++ components/farm/Farm_Stake.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const Farm_Stake = ({ setShowComponent }: Props) => {
-  const [stakeRange, setStakeRange] = useState(1);
+  const [stakeRange, setStakeRange] = useState(0);
 
   return (
     <React.Fragment>
@@ -34,65 +34,83 @@ const Farm_Stake = ({ setShowComponent }: Props) => {
       <p className="text-[14px] leading-[17px] font-medium w-full text-[#C6C6C6]">
         LP Tokens Available: <span className="text-white">11189.1994</span>
       </p>
-      <p className="text-white text-2xl font-medium pt-1">0</p>
+      <p className="text-white text-2xl font-medium pt-1">{stakeRange}</p>
       <div className="relative w-full">
         <input
           type="range"
           className="transparent h-1.5 slider w-full cursor-pointer appearance-none rounded-lg border-transparent bg-[#959595]"
-          min="1"
-          max="4"
+          min="0"
+          max="100"
           step="1"
           id="removeRange"
           value={stakeRange}
           onChange={(e) => setStakeRange(Number(e.target.value))}
         />
         <div
-          style={{ width: `${33.33 * (stakeRange - 1)}%` }}
-          className={`bg-[#1ED6FF] h-1.5 pointer-events-none absolute inset-0 top-1 my-auto rounded-full`}
+          style={{ width: `${1 * stakeRange}%` }}
+          className={`bg-[#268AFF] h-1.5 pointer-events-none absolute inset-0 top-1 my-auto rounded-full`}
         />
         <div className="w-full flex justify-between text-xs absolute top-1.5 pointer-events-none">
           <div
             style={{
-              background: `${stakeRange >= 1 ? "#1ED6FF" : "#D9D9D9"}`,
+              background: `${stakeRange >= 0 ? "#268AFF" : "#D9D9D9"}`,
             }}
             className="w-4 h-4 rounded-full"
           />
           <div
             style={{
-              background: `${stakeRange >= 2 ? "#1ED6FF" : "#D9D9D9"}`,
+              background: `${stakeRange >= 25 ? "#268AFF" : "#D9D9D9"}`,
             }}
             className="w-4 h-4 rounded-full"
           />
           <div
             style={{
-              background: `${stakeRange >= 3 ? "#1ED6FF" : "#D9D9D9"}`,
+              background: `${stakeRange >= 50 ? "#268AFF" : "#D9D9D9"}`,
             }}
             className="w-4 h-4 rounded-full"
           />
           <div
             style={{
-              background: `${stakeRange >= 4 ? "#1ED6FF" : "#D9D9D9"}`,
+              background: `${stakeRange >= 75 ? "#268AFF" : "#D9D9D9"}`,
+            }}
+            className="w-4 h-4 rounded-full"
+          />
+          <div
+            style={{
+              background: `${stakeRange >= 100 ? "#268AFF" : "#D9D9D9"}`,
             }}
             className="w-4 h-4 rounded-full"
           />
         </div>
       </div>
       <div className="w-full grid grid-cols-4 gap-4 text-[17px] leading-[20px] font-medium">
-        <button className=" px-6 py-4 small-component-color rounded-[14px] col-span-1">
+        <button
+          className={`px-6 py-4 small-component-color rounded-[14px] col-span-1`}
+          onClick={() => setStakeRange(25)}
+        >
           25%
         </button>
-        <button className=" px-6 py-4 small-component-color rounded-[14px] col-span-1">
+        <button
+          className={`px-6 py-4 small-component-color rounded-[14px] col-span-1`}
+          onClick={() => setStakeRange(50)}
+        >
           50%
         </button>
-        <button className=" px-6 py-4 small-component-color rounded-[14px] col-span-1">
+        <button
+          className={`px-6 py-4 small-component-color rounded-[14px] col-span-1`}
+          onClick={() => setStakeRange(75)}
+        >
           75%
         </button>
-        <button className=" px-6 py-4 small-component-color rounded-[14px] col-span-1">
+        <button
+          className={`px-6 py-4 small-component-color rounded-[14px] col-span-1`}
+          onClick={() => setStakeRange(100)}
+        >
           100%
         </button>
       </div>
       <div className=" w-full flex items-center gap-2 text-xs px-3 py-1">
-        <BsExclamationTriangleFill className="w-6 h-6 text-[#1ED6FF]" />
+        <BsExclamationTriangleFill className="w-6 h-6 text-[#268AFF]" />
         <p className="text-[12.7px] leading-[20px] font-medium">
           Each Stake or Withdraw action will automatically harvest your rewards
           for this pair!
